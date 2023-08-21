697b44f76d912c7a7bd2e7478081f2e7b9e744c8
NoteSwiper <ryuuking0410@gmail.com>
2023-08-21 23:25:26 +0900

    Added IDEA Options

--- src/main/java/io/github/noteswiper/reminecraft/reminecraft/Main.java
+++ src/main/java/io/github/noteswiper/reminecraft/reminecraft/Main.java
@@ -1,7 +1,5 @@
 package io.github.noteswiper.reminecraft.reminecraft;
 
-import org.lwjgl.*;
-
 /**
  * Hello world!
  *
@@ -12,7 +10,6 @@ public class Main
 	boolean random = true;
 	public static int blockcount = 0;
 	public int timer = 0;
-	Cursor
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );

de43696eeffcfd4f3bac8b1a2d834c58c42f6367
NoteSwiper <ryuuking0410@gmail.com>
2023-08-22 18:15:37 +0900

    1

--- src/main/java/io/github/noteswiper/reminecraft/reminecraft/Main.java
+++ src/main/java/io/github/noteswiper/reminecraft/reminecraft/Main.java
@@ -10,6 +10,7 @@ public class Main
 	boolean random = true;
 	public static int blockcount = 0;
 	public int timer = 0;
+	
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
